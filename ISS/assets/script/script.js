2bfbbe885afec63597d38bf0e5bc013d34442abc
OlivierSeill <olivier.seill@gmail.com>
2026-09-09 14:27:42 +0200

    ajout de commentaires supplémentaires

--- ISS/assets/script/script.js
+++ ISS/assets/script/script.js
@@ -30,8 +30,10 @@ const MARKER = L.marker([0, 0], {icon: ISS_ICON}).addTo(MAP);
 //Création fonction récupération de l'API & mis à jour de la carte et du marqueur
 async function issAPI(map,marker) {
   try {
+    //Contact API ISS
     const response = await fetch("http://api.open-notify.org/iss-now.json");
 
+    //Si un problème survient on s'assure d'en récupérer le code d'erreur HTTP
     if (!response.ok) {
       throw new Error(`Erreur de communication HTTP : ${response.status}`);
     }
@@ -46,6 +48,7 @@ async function issAPI(map,marker) {
     map.flyTo([latISS, longISS], 5);
     marker.setLatLng([latISS,longISS]);
 
+    //Si on a un problème avec le traitement des données API on s'assure d'avoir le message d'erreur
   } catch (erreur) {
     console.error("Impossible de charger les données de l'API:", erreur.message);
   }
@@ -53,7 +56,7 @@ async function issAPI(map,marker) {
 
 //Appel de la fonction toutes les 1s les secondes
 setInterval(()=>issAPI(MAP,MARKER),1000);
-//Attention on ne met pas la fonction issApi() directement enparamètre mais une fonction anonyme qui appelle la fonction. Sinon la valeur lue par la fonction setInterval() serait l'objet "promise" de la fonction asynchrone issAPI().
+//Attention on ne met pas la fonction issApi() directement en paramètre mais une fonction anonyme qui appelle celle-ci. Sinon la valeur lue par la fonction setInterval() serait l'objet "promise" de la fonction asynchrone issAPI(MAP,MARKER) appelé la première fois.
 
 /***************
   METEO LOCALE
